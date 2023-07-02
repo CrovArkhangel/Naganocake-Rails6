507db2e07c24c21de224461d6def22e676050b1e
CrovArkhangel <SaToShi.KR.1123@gmail.com>
2023-07-02 13:28:14 +0000

    Add default image

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ApplicationRecord
   def get_product_image(width, height)
     unless product_image.attached?
       file_path = Rails.root.join('app/assets/images/default-image.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      product_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     product_image.variant(resize_to_limit: [width, height]).processed
   end
